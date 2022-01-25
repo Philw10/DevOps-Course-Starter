5377faf004c86605d6e82a84eba8714aa838f48a
Phil Wilson <philipjwilson@gmail.com>
2022-01-25 12:28:53 +0000

    Tidy up and start of exercise 2

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from werkzeug.utils import redirect
-from todo_app.data.session_items import add_item, get_items
+from todo_app.data.session_items import add_item, get_item, get_items, save_item
 
 from todo_app.flask_config import Config
 
