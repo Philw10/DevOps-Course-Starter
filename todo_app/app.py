6c8fff948b37ca7d3926a96cf543a33c7777c2d5
Phil Wilson <philipjwilson@gmail.com>
2022-03-03 11:58:21 +0000

    ViewModel class added

--- todo_app/app.py
+++ todo_app/app.py
@@ -2,13 +2,15 @@ from flask import Flask, render_template, request, url_for
 from werkzeug.utils import redirect
 from todo_app.data.trello_items import complete_item, get_items, add_item
 from todo_app.flask_config import Config
+from todo_app.data.ViewModel import ViewModel
 
 app = Flask(__name__)
 app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return render_template('index.html', tasks = get_items())
+    task_view_model = ViewModel(get_items())
+    return render_template('index.html', view_model = task_view_model)
 
 @app.route('/new', methods=['POST'])
 def new_task():
