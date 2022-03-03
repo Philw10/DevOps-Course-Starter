b70377935aa1973fa2751d2541f1de3cc7ece986
Phil Wilson <philipjwilson@gmail.com>
2022-03-03 13:32:01 +0000

    Silght refactor

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -6,35 +6,22 @@ task_list = []
 def process_full_trello_records():
     
     items_dict = api_request_get()
-    process_todo_items(items_dict)
-    process_completed_items(items_dict)    
+    process_tasks(items_dict)
+       
+      
+def process_tasks(items_dict):
 
-        
-def process_todo_items(items_dict):
+    for i in range(len(items_dict)):
+        list_name = items_dict[i]['name']
+        for card in items_dict[i]['cards']:
+            duplicate_task = False
+            for task in task_list:
+                if (card['id'] == task.id):
+                    duplicate_task = True
+                    task.status = list_name   
+            if (duplicate_task == False):
+                add_card_to_task_list(card, list_name)               
 
-    for card in items_dict[0]['cards']:
-        duplicate_task = False
-        for task in task_list:
-            if (card['id'] == task.id):
-                duplicate_task = True
-                if (task.status == 'complete'):
-                    task.status = 'To Do'   
-        if (duplicate_task == False):
-            add_card_to_task_list(card, 'To Do')        
-            
-
-def process_completed_items(items_dict):
-
-    for card in items_dict[2]['cards']:
-        duplicate_task = False
-        for task in task_list:
-            if (card['id'] == task.id):
-                duplicate_task = True
-                if (task.status == 'To Do'):
-                    task.status = 'complete'                    
-        if (duplicate_task == False):  
-            add_card_to_task_list(card, 'complete')     
-            
 
 def add_card_to_task_list(card, status):
     new_task = Item.from_trello_cards(card, status)
