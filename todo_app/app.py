a24a5da8361cd8b159d671b9e9d67dae1338e3b6
Phil Wilson <philipjwilson@gmail.com>
2022-10-21 17:22:57 +0100

    Security integaration started

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,3 +1,6 @@
+import os
+import requests
+from flask_login import LoginManager, login_required
 from flask import Flask, render_template, request, url_for
 from werkzeug.utils import redirect
 from todo_app.data.mongo_items import get_items, add_item, doing_item, complete_item
@@ -8,22 +11,47 @@ def create_app():
         app = Flask(__name__)
         app.config.from_object(Config())
 
+        login_manager = LoginManager()
+
+        @login_manager.unauthorized_handler
+        def unauthenticated():
+                client_id = os.getenv('GITHUB_OAUTH_CLIENT_ID')
+                return redirect (f'https://github.com/login/oauth/authorize?client_id={client_id}')
+               
+        @login_manager.user_loader
+        def load_user(user_id):
+                pass # later
+
+        login_manager.init_app(app)
+
+        @app.route('/login/callback')
+        def validate_user():
+                code = request.args.get('code', '')
+                params = {'client_id': os.getenv('GITHUB_OAUTH_CLIENT_ID'), 'client_secret': os.getenv('GITHUB_OAUTH_SECRET'), 'code': code}
+                headers = {'Accept': 'application/json'}
+                get_access_token = requests.post(f'https://github.com/login/oauth/access_token', params=params, headers=headers).json()
+                access_token = get_access_token['access_token']
+
         @app.route('/')
+        @login_required
         def index():
                 task_view_model = ViewModel(get_items())
                 return render_template('index.html', view_model = task_view_model)
 
         @app.route('/new', methods=['POST'])
+        @login_required
         def new_task():
                 add_item(request.form.get('title'))
                 return redirect(url_for("index"))
 
         @app.route('/complete/<id>')
+        @login_required
         def complete_task(id):
                 complete_item(id)
                 return redirect(url_for("index"))
 
         @app.route('/doing/<id>')
+        @login_required
         def doing_task(id):
                 doing_item(id)
                 return redirect(url_for("index"))
