dee6cc88ead361b604e9966300f1598ac785326f
Phil Wilson <philipjwilson@gmail.com>
2022-10-31 11:45:11 +0000

    Authorization added

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,14 +1,20 @@
 import os
 import requests
-from flask_login import LoginManager, login_required
+from flask_login import LoginManager, login_required, UserMixin, login_user
 from flask import Flask, render_template, request, url_for
 from werkzeug.utils import redirect
 from todo_app.data.mongo_items import get_items, add_item, doing_item, complete_item
 from todo_app.flask_config import Config
 from todo_app.data.view_model import ViewModel
 
+class User(UserMixin):
+        def __init__(self, id):
+                self.id = id
+
+
 def create_app():
         app = Flask(__name__)
+        app.config['LOGIN_DISABLED'] = os.getenv('LOGIN_DISABLED') == 'True'
         app.config.from_object(Config())
 
         login_manager = LoginManager()
@@ -20,7 +26,7 @@ def create_app():
                
         @login_manager.user_loader
         def load_user(user_id):
-                pass # later
+                return User(user_id)
 
         login_manager.init_app(app)
 
@@ -28,9 +34,14 @@ def create_app():
         def validate_user():
                 code = request.args.get('code', '')
                 params = {'client_id': os.getenv('GITHUB_OAUTH_CLIENT_ID'), 'client_secret': os.getenv('GITHUB_OAUTH_SECRET'), 'code': code}
-                headers = {'Accept': 'application/json'}
-                get_access_token = requests.post(f'https://github.com/login/oauth/access_token', params=params, headers=headers).json()
-                access_token = get_access_token['access_token']
+                headers_for_token = {'Accept': 'application/json'}
+                access_token = (requests.post('https://github.com/login/oauth/access_token', params=params, headers=headers_for_token).json())['access_token']
+                           
+                headers_for_user = {'Authorization': f'Bearer {access_token}'}
+                user_details = requests.get('https://api.github.com/user', headers=headers_for_user).json()
+                id = User(user_details['id'])
+                login_user(id)
+                return redirect("/")
 
         @app.route('/')
         @login_required
